4d7986875e5a5d65fa781c617107ab76db57eaf6
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2023-02-17 13:40:53 -0300

    TOken + Authentication

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/jwt/service/JwtService.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/jwt/service/JwtService.java
@@ -14,14 +14,15 @@ import static org.springframework.util.ObjectUtils.isEmpty;
 @Service
 public class JwtService {
 
-    private static final String BEARER = "bearer";
+    private static final String EMPTY_SPACE = " ";
+    private static final Integer TOKEN_INDEX = 1;
 
-    @Value("${appi-config.secrets.api-secret}")
+    @Value("${app-config.secrets.api-secret}")
     private String apiSecret;
 
-    public void validateAuthorized(String token) {
+    public void validateAuthorization(String token) {
+        var accessToken = extractToken(token);
         try {
-            var accessToken = extractToken(token);
             var claims = Jwts
                     .parserBuilder()
                     .setSigningKey(Keys.hmacShaKeyFor(apiSecret.getBytes()))
@@ -43,9 +44,8 @@ public class JwtService {
         if (isEmpty(token)) {
             throw new AuthenticationException("The access token was not informed.");
         }
-        if (token.toLowerCase().contains(BEARER)) {
-            token = token.toLowerCase();
-            token = token.replace(BEARER, Strings.EMPTY);
+        if (token.contains(EMPTY_SPACE)) {
+            return token.split(EMPTY_SPACE)[TOKEN_INDEX];
         }
         return token;
     }
