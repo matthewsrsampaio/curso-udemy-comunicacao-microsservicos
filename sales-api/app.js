6b8104b37c404335c17ab65b26e8b1a228f4e4d7
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2023-03-06 13:37:27 -0300

    RabbitMQ configured and it's already working. Sales-API almost Done

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,15 +1,26 @@
-import express from 'express'
+import express from 'express';
+import { connectMongoDb } from "./src/config/db/mongoDbConfig.js";
+import { createInitialData } from './src/config/db/initialData.js'
+import checkToken from "./src/config/auth/checkToken.js";
+import { connectRabbitMq } from "./src/config/rabbitmq/rabbitConfig.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
-app.get("/api/status", (req, res) => {
+connectMongoDb();
+
+createInitialData();
+
+connectRabbitMq();
+
+app.use(checkToken);
+
+app.get("/api/status", async (req, res) => { // Isto aqui é um EndPoint
     return res.status(200).json({
         service: "Sales-API",
         httpStatus: 200,
         status: "up",
-        
     });
 });
 
---
+++ sales-api/src/config/constants/secrets.js
@@ -0,0 +1,7 @@
+const env = process.env;
+
+export const MONGO_DB_URL = env.MONGO_DB_URL ? env.MONGO_DB_URL : "mongodb://localhost:27017/sales-db"; //O banco está sem SENHA mesmo
+
+export const API_SECRET = env.API_SECRET ? env.API_SECRET : "YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=";
+
+export const RABBIT_MQ_URL = env.RABBIT_MQ_URL ? env.RABBIT_MQ_URL : "amqp://localhost:5672";
