858a0c4d726da55bbcd3ff51cb89116ae7d30b61
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2023-04-10 14:41:55 -0300

    - Ajustes para subir para a nuvem. - Ajustes de validacao da Api. - Exclusão do teste. Pq excluir? Pq nao vamos usar.

--- sales-api/src/config/constants/Secrets.js
+++ sales-api/src/config/constants/Secrets.js
@@ -1,9 +1,13 @@
 const env = process.env;
 
-export const MONGO_DB_URL = env.MONGO_DB_URL ? env.MONGO_DB_URL : "mongodb://localhost:27017/sales-db"; //O banco está sem SENHA mesmo
+export const MONGO_DB_URL = env.MONGO_DB_URL ? env.MONGO_DB_URL : "mongodb://localhost:27017/sales-db";
+// Endereço do banco local no docker. ->    "mongodb://localhost:27017/sales-db";   obs: //O banco está sem SENHA mesmo
+// Banco MONGODB Atlas (nuvem)        ->    "mongodb+srv://sales-db:sales-db@sales-db.l09pvkk.mongodb.net/?retryWrites=true&w=majority";
 
 export const API_SECRET = env.API_SECRET ? env.API_SECRET : "YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=";
 
-export const RABBIT_MQ_URL = env.RABBIT_MQ_URL ? env.RABBIT_MQ_URL : "amqp://localhost:5672";
+export const RABBIT_MQ_URL = env.RABBIT_MQ_URL ? env.RABBIT_MQ_URL : "amqps://fhwzffwb:7dUwT5K5EeK_HJK0WY0tD0HciB7loqpz@beaver.rmq.cloudamqp.com/fhwzffwb";
+// Lcal (docker)                      ->    "amqp://localhost:5672";
+// Cloud ampq (mensageria NUVEM)      ->    "amqps://fhwzffwb:7dUwT5K5EeK_HJK0WY0tD0HciB7loqpz@beaver.rmq.cloudamqp.com/fhwzffwb";
 
 export const PRODUCT_API_URL = env.PRODUCT_API_URL ? env.PRODUCT_API_URL : "http://localhost:8081/api/product";
